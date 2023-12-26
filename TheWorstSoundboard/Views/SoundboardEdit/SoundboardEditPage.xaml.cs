f8063db6a916a83064e5e4ddba6ddcb1222768e7
Sean Reilly <surreily1@gmail.com>
2023-12-26 19:02:02 +0000

    Update the soundboard edit page's UI to use circles instead of menu items for sounds.

--- TheWorstSoundboard/Views/SoundboardEdit/SoundboardEditPage.xaml.cs
+++ TheWorstSoundboard/Views/SoundboardEdit/SoundboardEditPage.xaml.cs
@@ -1,20 +1,77 @@
-using CommunityToolkit.Maui.Converters;
+using System.ComponentModel;
 using CommunityToolkit.Maui.Views;
 using Surreily.TheWorstSoundboard.Model;
+using Surreily.TheWorstSoundboard.Storage.Sound;
 
 namespace Surreily.TheWorstSoundboard.Views.SoundboardEdit {
     [QueryProperty(nameof(SoundboardName), "SoundboardName")]
     public partial class SoundboardEditPage : ContentPage {
+        private readonly ISoundStorage soundStorage;
+
         public string? SoundboardName {
             get => ViewModel.SoundboardName;
             set => ViewModel.SoundboardName = value;
         }
 
         public SoundboardEditPage(
-            SoundboardEditPageViewModel viewModel) {
+            SoundboardEditPageViewModel viewModel,
+            ISoundStorage soundStorage) {
 
             InitializeComponent();
             BindingContext = viewModel;
+
+            this.soundStorage = soundStorage;
+
+            ViewModel.PropertyChanged += ViewModel_PropertyChanged;
+        }
+
+        private void ViewModel_PropertyChanged(object? sender, PropertyChangedEventArgs e) {
+            if (e.PropertyName == nameof(ViewModel.SoundModels)) {
+                CreateSoundButtons();
+            }
+        }
+
+        private void CreateSoundButtons() {
+            SoundModelsFlexLayout.Clear();
+
+            foreach (SoundModel soundModel in ViewModel.SoundModels!) {
+                string soundName = Path.GetFileName(soundModel.SoundFileName!); // TODO: Have this in the SoundModel class
+                string extension = Path.GetExtension(soundModel.SoundFileName!);
+
+                Frame frame = new Frame {
+                    Background = Colors.AliceBlue,
+                    Margin = 5,
+                    HeightRequest = 90,
+                    WidthRequest = 90,
+                    CornerRadius = 45,
+                };
+
+                // TODO: This is not a good gesture to use, need something else here.
+                SwipeGestureRecognizer swipeGestureRecognizer = new SwipeGestureRecognizer {
+                    Direction = SwipeDirection.Right,
+                };
+
+                swipeGestureRecognizer.Swiped += async (sender, e) => {
+                    await NavigateToSoundEditPage(soundName);
+                };
+
+                TapGestureRecognizer tapGestureRecognizer = new TapGestureRecognizer();
+
+                tapGestureRecognizer.Tapped += (sender, e) => PlaySound(soundModel);
+
+                frame.GestureRecognizers.Add(tapGestureRecognizer);
+                frame.GestureRecognizers.Add(swipeGestureRecognizer);
+
+                if (soundModel.ImageFileName != null) {
+                    Image image = new Image {
+                        Source = new Uri(soundStorage.GetSoundFilePath(SoundboardName!, soundName, extension)),
+                    };
+
+                    frame.Content = image;
+                }
+
+                SoundModelsFlexLayout.Add(frame);
+            }
         }
 
         public SoundboardEditPageViewModel ViewModel => (SoundboardEditPageViewModel)BindingContext;
@@ -35,9 +92,13 @@ namespace Surreily.TheWorstSoundboard.Views.SoundboardEdit {
         }
 
         private async void AddButton_Clicked(object sender, EventArgs e) {
+            await NavigateToSoundEditPage();
+        }
+
+        private async Task NavigateToSoundEditPage(string? soundName = null) {
             Dictionary<string, object> parameters = new Dictionary<string, object> {
                 { "SoundboardName", SoundboardName! },
-                { "SoundName", "New Sound" },
+                { "SoundName", soundName ?? "New Sound" },
             };
 
             await Shell.Current.GoToAsync("Sounds/Edit", parameters);
